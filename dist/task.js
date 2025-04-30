385c55b4ba75b0f3184527923387d846be40505e
RadoslavNikolov23 <sitcom93@abv.bg>
2025-04-30 13:54:13 +0300

    Creating a new application for a random password generator using TypeScript and HTML/CSS. This application will generate a random password based on defined criteria such as length and character types (uppercase, lowercase, numbers, symbols).

---
+++ dist/task.js
@@ -0,0 +1,77 @@
+"use strict";
+const button = document.getElementById("generateButton");
+const input = document.getElementById("password");
+const passwordLength = document.getElementById("passwordLength");
+const message = document.getElementById("message");
+const messageWarning = document.getElementById("messageWarning");
+const resetButton = document.getElementById("resetButton");
+const copyClipboardButton = document.getElementById("copyClipboardButton");
+const strengthLevel = document.getElementById("strengthLevel");
+const toggle = document.getElementById("togglePassword");
+function passwordGenerator(lenght = 12) {
+    const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=";
+    let password = "";
+    for (let i = 0; i < lenght; i++) {
+        const index = Math.floor(Math.random() * char.length);
+        password += char[index];
+    }
+    return password;
+}
+function showPasswordStrength(lenght) {
+    let strength = 0;
+    let color = "red";
+    if (lenght >= 8)
+        strength = 33;
+    if (lenght >= 20)
+        strength = 66;
+    if (lenght >= 32)
+        strength = 99;
+    if (strength === 33)
+        color = "orange";
+    if (strength === 66)
+        color = "gold";
+    if (strength === 99)
+        color = "green";
+    strengthLevel.style.width = `${strength}%`;
+    strengthLevel.style.backgroundColor = color;
+}
+button.addEventListener("click", () => {
+    let length = parseInt(passwordLength.value, 12);
+    message.textContent = "";
+    if (isNaN(length)) {
+        message.textContent = "Please enter a number.";
+        return;
+    }
+    if (length < 8) {
+        messageWarning.textContent = "Password must be at least 8 characters. Using 8 instead.";
+        length = 8;
+    }
+    else if (length > 32) {
+        messageWarning.textContent = "Password can't be more than 32 characters. Using 32 instead.";
+        length = 32;
+    }
+    const password = passwordGenerator(length);
+    input.value = password;
+    showPasswordStrength(length);
+});
+resetButton.addEventListener("click", () => {
+    passwordLength.value = "12";
+    input.value = "";
+    message.textContent = "";
+    messageWarning.textContent = "";
+    strengthLevel.style.width = "0%";
+});
+copyClipboardButton.addEventListener("click", () => {
+    if (input.value != null) {
+        navigator.clipboard.writeText(input.value);
+        message.textContent = "Password copied to clipboard";
+        message.style.color = "black";
+    }
+    else {
+        message.textContent = "Generate first a password!";
+        message.style.color = "red";
+    }
+});
+toggle.addEventListener("change", () => {
+    input.type = toggle.checked ? "text" : "password";
+});
---
+++ src/task.ts
@@ -0,0 +1,83 @@
+const button = document.getElementById("generateButton") as HTMLBodyElement;
+const input = document.getElementById("password") as HTMLInputElement;
+const passwordLength = document.getElementById("passwordLength") as HTMLInputElement;
+const message = document.getElementById("message") as HTMLParagraphElement;
+const messageWarning = document.getElementById("messageWarning") as HTMLParagraphElement;
+const resetButton = document.getElementById("resetButton") as HTMLButtonElement;
+const copyClipboardButton = document.getElementById("copyClipboardButton") as HTMLButtonElement;
+const strengthLevel = document.getElementById("strengthLevel") as HTMLDivElement;
+const toggle = document.getElementById("togglePassword") as HTMLInputElement;
+
+function passwordGenerator(lenght: number = 12): string {
+    const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=";
+    let password = "";
+    for(let i=0;i<lenght;i++){
+        const index = Math.floor(Math.random()*char.length);
+        password+=char[index];
+    }
+    return password;
+}
+
+function showPasswordStrength(lenght:number){
+    let strength = 0;
+    let color = "red";
+
+    if(lenght>=8) strength = 33;
+    if(lenght>=20) strength = 66;
+    if(lenght>=32) strength = 99;
+
+    if(strength===33) color = "orange";
+    if(strength===66) color = "gold";
+    if(strength===99) color = "green";
+
+    strengthLevel.style.width=`${strength}%`;
+    strengthLevel.style.backgroundColor=color;
+
+}
+
+button.addEventListener("click",() =>{
+    let length = parseInt(passwordLength.value,12);
+    message.textContent = "";
+
+    if (isNaN(length)) {
+        message.textContent = "Please enter a number.";
+        return;
+    }
+
+    if(length<8){
+        messageWarning.textContent = "Password must be at least 8 characters. Using 8 instead.";
+        length=8;
+    }    
+    else if(length>32){
+        messageWarning.textContent = "Password can't be more than 32 characters. Using 32 instead.";
+        length = 32;
+    } 
+
+    const password = passwordGenerator(length);
+    input.value=password
+    showPasswordStrength(length);
+});
+
+resetButton.addEventListener("click", () => {
+    passwordLength.value = "12";  
+    input.value = "";             
+    message.textContent = "";
+    messageWarning.textContent = "";
+    strengthLevel.style.width = "0%";    
+});
+
+copyClipboardButton.addEventListener("click", ()=>{
+    if(input.value !=null){
+        navigator.clipboard.writeText(input.value);
+        message.textContent = "Password copied to clipboard";
+        message.style.color="black"
+    }
+    else{
+        message.textContent="Generate first a password!";
+        message.style.color="red";
+    }
+});
+
+toggle.addEventListener("change", () => {
+    input.type = toggle.checked ? "text" : "password";
+});
